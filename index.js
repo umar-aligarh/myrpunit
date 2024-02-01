c84b758515b88f8e80b869d4716629f23594190b
Umar Khan <95311753+umar-aligarh@users.noreply.github.com>
2024-02-01 22:22:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const port = process.env.PORT || 5000;
 //     res.send("hello world");
 // });
 
-app.get("*", function (_, res) {
+app.get("/", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "/frontend/dist/index.html"),
+    path.join(__dirname, "/frontend/index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err);
